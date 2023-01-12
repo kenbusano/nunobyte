bacaa094a0b59ac7a9ea758afcc130a6c252d72d
lzynth0506 <kenbusano2003@gmail.com>
2023-01-13 02:00:25 +0800

    There's still changes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Art from "../components/Art";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Status from "../components/Status";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Art />
+      <Footer />
     </>
   )
 }
