c3341fceba17cd99f55627be0e18c5d013231b44
lzynth0506 <kenbusano2003@gmail.com>
2023-01-13 23:18:16 +0800

    Game options :wink:

--- components/utils/Option.js
+++ components/utils/Option.js
@@ -4,7 +4,7 @@ export default function Option() {
     return (
         <div className="fixed left-0 right-0 bottom-0 p-4 text-white opacity-30">
             <div className="flex items-center justify-between lg:container lg:mx-auto">
-                <button id="sound-mode" className="text-6xl">
+                <button id="sound-mode" className="sm:text-4xl lg:text-6xl">
                     <BsVolumeUpFill />
                 </button>
                 <div id="latest-version" className="text-2xl uppercase">
