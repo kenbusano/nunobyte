b0fc39c45bfd9217c2f127fb3f9d5800e33acffd
lzynth0506 <kenbusano2003@gmail.com>
2023-01-20 16:44:22 +0800

    Sound button remove :sad:

--- components/utils/Option.js
+++ components/utils/Option.js
@@ -1,23 +1,7 @@
-import {useState, useRef, useEffect} from "react"
-import { BsVolumeUpFill, BsVolumeMuteFill } from "react-icons/bs"
-
-export default function Option(handleLoad, handlePlay, handleFinish) {
-    const [soundMode, setSoundMode] = useState(false) //icon change
-    const [audio, setAudio] = useState(null) //set default
-
-    //Very-needed to work Audio property.
-    useEffect(() => {
-        setAudio(new Audio("./audio/nunobyte.mp3"))
-    }, [])
+export default function Option() {
     return (
         <div className="fixed left-0 right-0 bottom-0 p-4 text-white opacity-30">
-            <div className="flex items-center justify-between lg:container lg:mx-auto">
-                <button
-                    id="sound-mode" className="sm:text-4xl lg:text-6xl"
-                    onClick={() => setSoundMode(!soundMode) || audio.play()}
-                >
-                    {!soundMode ? <BsVolumeUpFill /> : <BsVolumeMuteFill />}
-                </button>
+            <div className="flex items-center justify-center lg:container lg:mx-auto">
                 <div id="latest-version" className="text-2xl uppercase">
                     <span>Version: 0.5.0 (Beta)</span>
                 </div>
