0688741a9728b550b0ade09b08bf8b051e4484f4
lzynth0506 <kenbusano2003@gmail.com>
2023-01-20 19:50:32 +0800

    SEO added :rocket:

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,22 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* all SEO starters must be here 💛 */}
-        <title>nunobyte</title>
+          {/* Open Graph on Facebook */}
+          <meta property="og:url" content="" />
+          <meta property="og:type" content="quiz game" />
+          <meta property="og:title" content="Nunobyte" />
+          <meta property="og:description" content="It's just a quiz game!" />
+          <meta property="og:image" content="/night_fire_by_waltjan_dejibyo-pre.jpg" />
+          {/* Twitter Card */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="" />
+          <meta property="twitter:url" content="" />
+          <meta name="twitter:title" content="Nunobyte" />
+          <meta name="twitter:description" content="It's just a quiz game!" />
+          <meta name="twitter:image" content="/night_fire_by_waltjan_dejibyo-pre.jpg" />
+          {/* Normal Meta Tag */}
+          <meta name="description" content="It's just a game!" />
+          <title>nunobyte</title>
       </Head>
       <main className={nunofont.className}>
         <Component {...pageProps} />
