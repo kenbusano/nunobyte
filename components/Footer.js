6f6fde85edc5132373e9a7aafce953ecf8502347
lzynth0506 <kenbusano2003@gmail.com>
2023-01-20 19:26:44 +0800

    Add some link components

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import { FaDev, FaGithub } from 'react-icons/fa'
-
+import Link from "next/link"
 export default function Footer() {
     return (
         <footer className="text-white bg-art sm:p-6 lg:p-12 text-center">
@@ -7,8 +7,12 @@ export default function Footer() {
                 <span className='mb-2 block'>Create by Kenneth Obsequio and this project is <b>Open Source</b> under <b>MIT License</b></span>
                 <p className='mb-4'>Therefore, do you like that? give it star on his GitHub repository</p>
                 <div className='inline-flex text-4xl'>
-                    <FaDev className='mr-4' />
-                    <FaGithub />
+                    <Link href="https://dev.to/kenbusano" target={"_blank"}>
+                        <FaDev className='mr-4' />
+                    </Link>
+                    <Link href="https://github.com/kenbusano/" target={"_blank"}>
+                        <FaGithub />
+                    </Link>
                 </div>
             </div>
         </footer>
--- components/Status.js
+++ components/Status.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 export default function Status() {
     return (
-        <section className="flex items-center justify-center uppercase text-center text-white sm:p-6 lg:p-0 lg:pb-24">
-            <h2 className="font-primetime sm:text-2xl md:text-4xl lg:text-6xl lg:max-w-6xl">for more details kindly check our status</h2>
-        </section>
+        <Link href="https://github.com/kenbusano/nunobyte" target={"_blank"}>
+            <section className="flex items-center justify-center uppercase text-center text-white sm:p-6 lg:p-0 lg:pb-24">
+                <h2 className="font-primetime sm:text-2xl md:text-4xl lg:text-6xl lg:max-w-6xl">for more details kindly check our status</h2>
+            </section>
+        </Link>
     )
 }
